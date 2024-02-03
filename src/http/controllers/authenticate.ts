5bb33c4629f403ae10bc2d7929933b5639d568a2
Pedro Veras <veraspedro1608@gmail.com>
2024-02-03 17:27:15 -0300

    refactor: :recycle: create folder for schema validations

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,17 +1,12 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { z } from 'zod'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+import { authenticateBodySchema } from '@/validations/authenticate-schema'
 
 export async function authenticate(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const authenticateBodySchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-
   const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
