f19d046a838350f00ca2e62b1be7ca3fb3ec7f5c
Pedro Veras <veraspedro1608@gmail.com>
2024-02-04 17:10:32 -0300

    refactor: :recycle: send error message when user not found in get user profile controller

--- src/http/controllers/user/get-user-profile.ts
+++ src/http/controllers/user/get-user-profile.ts
@@ -1,20 +1,29 @@
-import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { UserNotFoundError } from '@/use-cases/errors/user-not-found-error'
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 
 export async function getUserProfile(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const getUserProfile = makeGetUserProfileUseCase()
+  try {
+    const getUserProfile = makeGetUserProfileUseCase()
 
-  const { user } = await getUserProfile.execute({
-    userId: request.user.sub,
-  })
+    const { user } = await getUserProfile.execute({
+      userId: request.user.sub,
+    })
 
-  return reply.status(200).send({
-    data: {
-      ...user,
-      password: undefined,
-    },
-  })
+    return reply.status(200).send({
+      data: {
+        ...user,
+        password: undefined,
+      },
+    })
+  } catch (error) {
+    if (error instanceof UserNotFoundError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
+  }
 }
