519548671ed0766fe9b3ba514a3f3a17d0c838e5
Pedro Veras <veraspedro1608@gmail.com>
2024-03-18 20:24:53 -0300

    feat: ✨ edit user e2e test

---
+++ src/http/controllers/user/edit-user.spec.ts
@@ -0,0 +1,46 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Edit User (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to edit an user', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body
+
+    const userEdit = {
+      name: 'Pedro Veras',
+      email: 'pedroveras@gmail.com',
+    }
+
+    await request(app.server)
+      .patch('/user')
+      .set('Authorization', `Bearer ${token}`)
+      .send(userEdit)
+
+    const profileResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body.data.name).toEqual(userEdit.name)
+  })
+})
