73e2bea65be424bde5fd9d5b8036cbbae272194a
Pedro Veras <veraspedro1608@gmail.com>
2024-03-18 20:46:28 -0300

    refactor: ♻  changing name of user controller files

--- src/http/routes/user-routes.ts
+++ src/http/routes/user-routes.ts
@@ -2,11 +2,11 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '../middlewares/verify-jwt'
 
-import { register } from '../controllers/user/register'
-import { editUser } from '../controllers/user/edit-user'
-import { deleteUser } from '../controllers/user/delete-user'
-import { fetchAllUsers } from '../controllers/user/fetch-all-users'
-import { getUserProfile } from '../controllers/user/get-user-profile'
+import { register } from '../controllers/user/register.controller'
+import { editUser } from '../controllers/user/edit-user.controller'
+import { deleteUser } from '../controllers/user/delete-user.controller'
+import { fetchAllUsers } from '../controllers/user/fetch-all-users.controller'
+import { getUserProfile } from '../controllers/user/get-user-profile.controller'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
