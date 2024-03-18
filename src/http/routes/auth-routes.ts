0d6dcfa86579d7ceb7ab090fe218f69e56cee3b6
Pedro Veras <veraspedro1608@gmail.com>
2024-03-18 20:49:49 -0300

    refactor: ♻  changing name of auth controller files

--- src/http/routes/auth-routes.ts
+++ src/http/routes/auth-routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import { authenticate } from '../controllers/auth/authenticate'
+import { authenticate } from '../controllers/auth/authenticate.controller'
 
 export async function authRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
